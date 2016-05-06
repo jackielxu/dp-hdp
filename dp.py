72f2e01593809fb84fda84a80a2d083593b531d0
Surya Bhupatiraju <surya95@gmail.com>
2016-05-06 11:09:09 -0400

    Matching Jackie's comments/tabs

--- dp.py
+++ dp.py
@@ -6,7 +6,7 @@ import random
 
 # TODO: Write a DP (Chinese Restaurant Process)
 
-def crp(a, n=200):
+def crp(alpha, n=200):
   """
   Generates data via Chinese Restaurant Process. 
   
@@ -30,66 +30,71 @@ def crp(a, n=200):
       out.append(choice)
   return out
 
-def sbp(alpha, n=30):
-    """
-    Generative process that simulates the stick breaking process.
+def sbp(alpha, n=200):
+  """
+  Generative process that simulates the stick breaking process.
 
-    alpha:  concentration/disperson parameter; roughly correlates to how spread
-            out we want our diversity of stick lengths to be.  
+  Input
+  alpha : concentration/disperson parameter; roughly correlates to how spread
+          out we want our diversity of stick lengths to be.  
+      n : number of iterations to continue the stick breaking 
 
-        n:  number of iterations to continue the stick breaking 
+  Output
+  out : output distribution
+  """
 
-    """
+  out = [] 
+  draws = iter(np.random.beta(1, alpha, n))
+  pi_k = 1
+  stk = 1
 
-    out = [] 
-    draws = iter(np.random.beta(1, alpha, n))
-    pi_k = 1
-    stk = 1
+  for i in range(n):
+    b_k = next(draws)
+    pi_k = b_k * stk
+    out.append(pi_k) 
+    stk = stk * (1 - b_k)
 
-    for i in range(n):
-        b_k = next(draws)
-        pi_k = b_k * stk
-        out.append(pi_k) 
-        stk = stk * (1 - b_k)
-
-    return out
+  return out
 
 
 def pu(alpha, H, n=100):
-    """
-    Generative process that simulates a Polya Urn scheme. We index the colors
-    in both the H and G urns by integers. 
+  """
+  Generative process that simulates a Polya Urn scheme. We index the colors
+  in both the H and G urns by integers. 
 
-    alpha:  concentration/dispersion parameter; roughly correlates to how spread
-            out we want our diversity of colors to be. 
+  Input
+  alpha:  concentration/dispersion parameter; roughly correlates to how spread
+          out we want our diversity of colors to be. 
 
-        H:  base distribution; specifies a distribution for an underlying color
-            scheme that is sampled at a rate proportional to alpha. 
+      H:  base distribution; specifies a distribution for an underlying color
+          scheme that is sampled at a rate proportional to alpha. 
 
-            *NOTE*: distribution must be one from np.random, such as 
-            np.random.normal, or np.random.beta, and the size parameter must 
-            match n.
+          *NOTE*: distribution must be one from np.random, such as 
+          np.random.normal, or np.random.beta, and the size parameter must 
+          match n.
 
-        n:  number of iterations to draw samples from an urn in one run.
+      n:  number of iterations to draw samples from an urn in one run.
 
-    """
-    
-    H_draws = iter(H)
-    out = [sample(H_draws)]
-    alpha = float(alpha)
+  Output
+  out : output distribution
+  """
+  
+  H_draws = iter(H)
+  out = [sample(H_draws)]
+  alpha = float(alpha)
 
-    for i in range(n):
-        rand = random.random()
-        if rand < alpha/(alpha + i):
-            nxt = sample(H_draws)
-            out.append(nxt)
-        else:
-            out.append(random.choice(out))
+  for i in range(n):
+    rand = random.random()
+    if rand < alpha/(alpha + i):
+      nxt = sample(H_draws)
+      out.append(nxt)
+    else:
+      out.append(random.choice(out))
 
-    return out
+  return out
 
 def sample(H):
-    return int(max(round(next(H)), 0)) #TODO: Should this be max of the number and 0?
+  return int(max(round(next(H)), 0)) #TODO: Should this be max of the number and 0?
 
 crp_out = crp(10)
 #plt.hist(out)
